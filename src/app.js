afe838bab04ccfe2a3ac5d7ee3be5f0c7492e635
LitvinaEvelinova <hesitatingunicorn@gmail.com>
2022-09-10 20:35:55 +0200

    added a search engine

--- src/app.js
+++ src/app.js
@@ -32,8 +32,20 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
+    let apiKey = "9f19ccbb495d9cacd63f236963dafe78";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-let apiKey = "9f19ccbb495d9cacd63f236963dafe78";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Mariupol&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
-    
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("Mariupol");
